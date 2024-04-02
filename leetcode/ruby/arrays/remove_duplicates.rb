ce2538a51f1defa0509a93e48ac41e1c5dc2f31e
Lucas Moreira <lucasmsea@outlook.com>
2024-04-02 20:52:52 -0300

    feat: add new questions solved w/ ruby

---
+++ leetcode/ruby/arrays/remove_duplicates.rb
@@ -0,0 +1,19 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+  return 0 if nums.empty?
+
+  index_of_unique_numbers = 1
+
+  (1...nums.length).each do |index|
+    current_number = nums[index]
+    last_number = nums[index - 1]
+
+    if current_number != last_number
+      nums[index_of_unique_numbers] = current_number
+      index_of_unique_numbers += 1
+    end
+  end
+
+  index_of_unique_numbers
+end
---
+++ leetcode/ruby/arrays/remove_element.rb
@@ -0,0 +1,15 @@
+def remove_element(nums, val)
+  number_of_non_val_values = 0
+  index_of_filtered_array = 0
+
+  nums.each_index do |index|
+    number = nums[index]
+    next unless number != val
+
+    nums[index_of_filtered_array] = nums[index]
+    index_of_filtered_array += 1
+    number_of_non_val_values += 1
+  end
+
+  number_of_non_val_values
+end
