c7a1362ade99deaa23544832a43c8a3ba3e9450a
Lucas Moreira <lucasmsea@outlook.com>
2024-04-09 09:57:08 -0300

    question: finish remove duplicates from sorted list

---
+++ leetcode/ruby/linked_lists/remove_duplicates_from_sorted_list.rb
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val = 0, _next = nil)
+    @val = val
+    @next = _next
+  end
+end
+
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+  current_head = head
+
+  while current_head
+    if current_head.next
+      non_repeated_element = current_head
+
+      while non_repeated_element.next && non_repeated_element.next.val == non_repeated_element.val
+        non_repeated_element = non_repeated_element.next
+      end
+
+      current_head.next = non_repeated_element.next
+      current_head = non_repeated_element.next
+    else
+      current_head = current_head.next
+    end
+  end
+
+  head
+end
