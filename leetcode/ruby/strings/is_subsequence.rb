9d859d3af90c394371e03106aeccf8fd3bf7cec7
Lucas Moreira <lucasmsea@outlook.com>
2024-04-12 11:33:14 -0300

    question: finish `is_subsequence`

---
+++ leetcode/ruby/strings/is_subsequence.rb
@@ -0,0 +1,18 @@
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_subsequence(s, t)
+  return true if s.length.zero?
+
+  s_pointer = 0
+
+  t.each_char do |t_character|
+    if s_pointer == s.length
+      return true
+    elsif s[s_pointer] == t_character
+      s_pointer += 1
+    end
+  end
+
+  !(s_pointer < s.length)
+end
